9a559778cf6ea177c9319106a7bfa14252c5467b
Matheus Perrut <matheus.perrut@govbr.com.br>
2023-02-06 20:35:00 -0300

    - Criação projeto backend

---
+++ controle-projetos/src/test/java/perrut/matheus/controleprojetos/ControleProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package perrut.matheus.controleprojetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
